4aba19e171aef8b78804912e2dc4cc95eeb68409
kenokenobingo <mail.kewe@gmail.com>
2018-06-15 17:48:41 +0200

    macso

---
+++ Meme/meme/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  meme
+//
+//  Created by K E N O  on 15.06.18.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
